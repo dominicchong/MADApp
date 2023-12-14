51ff0446098536b227c3f8adc6ac5987d87df4d1
dominicchong <rydominicchong@gmail.com>
2023-12-14 21:06:12 +0800

    Integrated Login module with the whole app

---
+++ app/src/main/java/com/example/madapp/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.madapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
