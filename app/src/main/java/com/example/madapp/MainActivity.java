2cfbcbaaf08de97acb452b40afea3dff7aaff752
GohYH23 <121934266+GohYH23@users.noreply.github.com>
2023-12-14 11:16:49 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/madapp/MainActivity.java
+++ app/src/main/java/com/example/madapp/MainActivity.java
@@ -21,7 +21,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    //demo 123
+    //demo
     private ActionBarDrawerToggle toggle;
     private DrawerLayout drawerLayout;
 
