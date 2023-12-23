e962c5834c04ce9294d1be0209422d5caa4a3a75
dominicchong <rydominicchong@gmail.com>
2023-12-23 21:16:19 +0800

    Updated quiz feature

--- app/src/main/java/com/example/madapp/quiz/QuizScoreFragment.java
+++ app/src/main/java/com/example/madapp/quiz/QuizScoreFragment.java
@@ -1,22 +1,21 @@
-package com.example.madapp;
+package com.example.madapp.quiz;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
+import com.example.madapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link QuizFragment#newInstance} factory method to
+ * Use the {@link QuizScoreFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class QuizFragment extends Fragment {
+public class QuizScoreFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +26,7 @@ public class QuizFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public QuizFragment() {
+    public QuizScoreFragment() {
         // Required empty public constructor
     }
 
@@ -37,11 +36,11 @@ public class QuizFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DestQuiz.
+     * @return A new instance of fragment QuizScoreFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static QuizFragment newInstance(String param1, String param2) {
-        QuizFragment fragment = new QuizFragment();
+    public static QuizScoreFragment newInstance(String param1, String param2) {
+        QuizScoreFragment fragment = new QuizScoreFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,27 +61,6 @@ public class QuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
-    }
-
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        Button BtnQuiz1 = view.findViewById(R.id.BtnStartQuiz);
-        View.OnClickListener OCLArticle = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.DestQuizIn);
-            }
-        };
-        BtnQuiz1.setOnClickListener(OCLArticle);
-
-        Button BtnQuiz3 = view.findViewById(R.id.BtnScoreQuiz);
-        View.OnClickListener OCLMap = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.DestScoreQuiz);
-            }
-        };
-        BtnQuiz3.setOnClickListener(OCLMap);
-
+        return inflater.inflate(R.layout.fragment_quiz_score, container, false);
     }
 }
